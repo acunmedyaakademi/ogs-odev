9891f45a5e7affc5fa043d574f4f42f13acfbc79
samet özcan <sametozcan544@gmail.com>
2023-07-16 15:56:35 +0300

    proje açıldı ve versiyonlar oluşturuldu

---
+++ ogs ödev/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ogs_ödev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ ogs ödev/versıon-2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ogs_ödev
+{
+    internal class versıon_2
+    {
+    }
+}
