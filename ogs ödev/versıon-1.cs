b2f047844dac6ebce32365e14ca017fb4a3e197b
Öykü Zümrüt <oyku_zumrut@hotmail.com>
2023-07-21 16:09:42 +0300

    deneme

--- ogs ödev/versıon-1.cs
+++ ogs ödev/versıon-1.cs
@@ -1,17 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ogs_ödev
 {
     internal class versıon_1
     {
-        public giderKaydi()
+        class Gider
         {
-            //gider kayıt hesaplama 
+            public string Ad { get; set; }
+            public decimal Miktar { get; set; }
+
+            public Gider(string ad, decimal miktar)
+            {
+                Ad = ad;
+                Miktar = miktar;
+            }
+        
+        public static void giderGiris(List<Gider> giderler) 
+        {
+            
+            Console.WriteLine("Gider adını girin: ");
+            string ad = Console.ReadLine();
+
+            Console.WriteLine("Gider miktarını girin: ");
+            decimal miktar = Convert.ToDecimal(Console.ReadLine());
+
+            Gider yeniGider = new Gider(ad, miktar);
+            giderler.Add(yeniGider);
+
+            Console.WriteLine("Gider kaydedildi.");
         }
+       
+        public static void giderToplam(List<Gider> giderler)
+        {
+            Console.WriteLine("----- Kaydedilen Giderler -----");
+            foreach (Gider gider in giderler)
+            {
+                Console.WriteLine($"Gider Adı: {gider.Ad}, Miktar: {gider.Miktar}");
+            }
+        }
+        public static void vergiHesap()
+        {
+            //vergi hesaplama kod 
+        }
+
 
         static void Main(string[] args)
         {
@@ -21,41 +53,40 @@ namespace ogs_ödev
             {
                 Console.WriteLine("----- MENÜ -----");
 
-                Console.WriteLine("1. Gider Kaydı");
+                Console.WriteLine("1. Gider Girişi");
 
-                Console.WriteLine("2. Gider Girişi");
-               
-                Console.WriteLine("3. Gideler Toplamı");
-                
-                Console.WriteLine("4. Vergi Hesaplama");
-                
-                Console.WriteLine("5. Çıkış");
-                
+                Console.WriteLine("2. Giderler Toplamı");
+
+                Console.WriteLine("3. Vergi Hesaplama");
+
+                Console.WriteLine("4. Çıkış");
+
                 Console.WriteLine("-----------------");
-                
+
                 Console.WriteLine("Lütfen seçiminizi yapın: ");
 
                 int secim = Convert.ToInt32(Console.ReadLine());
 
                 switch (secim)
                 {
+                   
+
                     case 1:
-                        Console.WriteLine("Gider Kaydı menüsüne hoş geldiniz.");
+                        Console.WriteLine("Gider Girişi menüsüne hoş geldiniz.");
+                        giderGiris();
                         break;
 
                     case 2:
-                        Console.WriteLine("Gider Girişi menüsüne hoş geldiniz.");
+                        Console.WriteLine("Giderler Toplamı menüsüne hoş geldiniz.");
+                        giderToplam();
                         break;
 
                     case 3:
-                        Console.WriteLine("Gideler Toplamı menüsüne hoş geldiniz.");
+                        Console.WriteLine("Vergi Hesaplama menüsüne hoş geldiniz.");
+                        vergiHesap();
                         break;
 
                     case 4:
-                        Console.WriteLine("Vergi Hesaplama menüsüne hoş geldiniz.");
-                        break;
-
-                    case 5:
                         Console.WriteLine("Çıkış yapılıyor...");
                         devam = false;
                         break;
@@ -64,7 +95,7 @@ namespace ogs_ödev
                         Console.WriteLine("Geçersiz bir seçim yaptınız. Lütfen tekrar deneyin.");
                         break;
                 }
-                 
+
             }
         }
 
